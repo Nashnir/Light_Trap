0a34bdb13ad34ffeb232f03276d762b270905481
Sebastien Gaumin <sgaumin@gmail.com>
2018-12-28 22:55:40 +0100

    First prototype scene
    Character movement 2D + basic interactions

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Supply[] supplies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        supplies = FindObjectsOfType<Supply>();
+    }
+
+    bool CheckSupplyIsCollected() {
+        bool allSuppliesCollected = true;
+
+        foreach (Supply supply in supplies)
+        {
+            if (!supply.isCollected)
+            {
+                allSuppliesCollected = false;
+                break;
+            }
+        }
+
+        return allSuppliesCollected;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (CheckSupplyIsCollected())
+            {
+                GameManager.instance.ReloadScene();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Supply.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Supply : MonoBehaviour
+{
+    [SerializeField] public bool isCollected = false;
+
+    private SpriteRenderer sprite;
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isCollected = true;
+            sprite.enabled = false;
+        }
+    }
+}
